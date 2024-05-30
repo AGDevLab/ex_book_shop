818f1eb9143b3f5d46b533de7799164c5c0b90fd
Amit Gal <amitgal@hotmail.com>
2024-05-30 20:47:48 +0300

    Add html table js css

--- js/bookshop.js
+++ js/bookshop.js
@@ -1,1 +1,30 @@
 'use strict'
+
+var gBooks = {
+  id: 'bg4J78',
+  title: 'The adventures of Lori Ipsi',
+  price: 120,
+  imgUrl: 'lori-ipsi.jpg',
+}
+
+function onInit() {
+  console.log('onInit')
+}
+
+function renderBooks() {}
+
+function getBooks() {}
+
+function onRemoveBook(bookId) {
+  console.log('book removed')
+}
+
+function removeBook() {}
+
+function onUpdateBook() {}
+
+function updatePrice() {}
+
+function onAddBook() {
+  console.log('Adding book')
+}
