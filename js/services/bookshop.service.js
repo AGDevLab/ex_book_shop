c71ee4bc77e568a7e8b3a851672b4ecc1b0943ec
Amit Gal <amitgal@hotmail.com>
2024-06-01 03:13:49 +0300

    Add local storage updating

--- js/services/bookshop.service.js
+++ js/services/bookshop.service.js
@@ -5,31 +5,48 @@ var gBooks = _createBooks('bookshelf')
 // loadFromStorage('bookshelf')
 
 function getBooks(bookId) {
-  const book = gBooks.find((book) => book.id === bookId)
+  const book = getBook(bookId)
+  // const book = gBooks.find((book) => book.id === bookId)
   return JSON.stringify(book, null, 2)
+}
+
+function getBook(bookId) {
+  return gBooks.find((book) => book.id === bookId)
+}
+
+function findBookByName(titleStr) {
+  return gBooks.find((book) => book.title === titleStr)
+}
+
+function findBookPrice(priceStr) {
+  return gBooks.find((book) => book.price === priceStr)
 }
 
 function addBook(txt) {
   const book = _createBook(txt)
   gBooks.unshift(book)
+  _saveBooks()
 }
 
 function removeBook(bookId) {
   const idx = gBooks.findIndex((book) => book.id === bookId)
   gBooks.splice(idx, 1)
+  _saveBooks()
 }
 
 function updatePrice(bookId) {
   var priceChange = +prompt('please choose a price')
   const idx = gBooks.findIndex((book) => book.id === bookId)
   gBooks[idx].price = priceChange
+  _saveBooks()
 }
 
 function _createBook(txt) {
   return {
-    id: `${Date.now() % 1000}`,
+    id: makeId(),
     title: txt,
     price: '',
+    // price: `${findBookByName(txt).price}`,
     imgUrl: `returnnull.jpg`,
   }
 }
@@ -63,9 +80,13 @@ function _createBooks(key) {
     },
   ]
 }
-_saveBooks()
+// _saveBooks()
 
 function _saveBooks() {
+  saveToStorage('bookshelf', gBooks)
+}
+
+function _readBooks() {
   const tempStr = loadFromStorage('bookshelf')
   const tempData = JSON.parse(tempStr)
   console.log(tempData)
